4f6c9888a7cb3f90e2432f126bbbd6d77cc20593
cc <38369051+Holyfiled@users.noreply.github.com>
2021-03-01 18:41:56 +0800

    Update kafka_admin_client.py
    1. 获取kafka消费组、消费者以及消费者对应的Topic。

--- kafka_admin_client.py
+++ kafka_admin_client.py
@@ -1,44 +1,53 @@
 # -*- coding: utf-8 -*-
+# pip 
 from kafka.admin import KafkaAdminClient
+
 
 
 BOOTSTRAP_SERVERS = '192.168.32.128:9092'
 kafka_client = KafkaAdminClient(bootstrap_servers=BOOTSTRAP_SERVERS)
 
 
-def get_kafka_consumergrouplist_list():
+def get_kafka_consumergroup():
     consumergrouplist = kafka_client.list_consumer_groups()
     consumer_group = []
-    print(type(consumergrouplist), consumergrouplist)
     for group_name, consumer_flag in consumergrouplist:
         if 'consumer' in consumer_flag:
             consumer_group.append(group_name)
-            get_consumergroup_topic(consumer_group)
+    print('ConsumerGroup list: {}'.format(consumer_group))
+    return consumer_group
 
 
-def get_consumergroup_topic(group_id):
-    consumer_group_info = kafka_client.describe_consumer_groups(group_id)
+def get_consumer_client_host():
+    consumer_and_topic = dict()
+    consumer_host_ip = set()
+    consumer_group = get_kafka_consumergroup()
+    consumer_group_info = kafka_client.describe_consumer_groups(consumer_group)
     for line in consumer_group_info:
-        print("Consumer group: %s" % (line.group))
-        consumer_member = line.members
-        print(consumer_member)
-        for member in consumer_member:
-            print("Consumer IP: %s" % (member.client_host))
-            for topic in member.member_metadata.subscription:
-                print(topic)
-
-'''
-dic={consumer_goup:[{ip1:{topic1,topic1,topic2,....},{ip2:{topic1,topic2,....},{ip3:{topic1,topic2,....}]}
-for v in dic.values():
-   for v1 in v.values():
-      v1.add()
-'''
+        for consumer_member in line.members:
+            topics = set()
+            consumer_ip = consumer_member.client_host
+            consumer_host_ip.add(consumer_member.client_host)
+            for topic in consumer_member.member_metadata.subscription:
+                topics.add(topic)
+            if consumer_ip in consumer_and_topic.keys():
+                consumer_and_topic[consumer_ip] = topics | consumer_and_topic[consumer_ip]
+            else:
+                consumer_and_topic[consumer_ip] = topics
+    return consumer_and_topic
 
 
+def get_consumers_topics(consumer_dict):
+    print('Consumer list is: {}'.format(consumer_dict.keys()))
+    print('consumer\'s topics list is:')
+    for consumer_ip, topics in consumer_dict.items():
+        print(consumer_ip)
+        for topic in topics:
+            print(topic)
 
 
 def main():
-    get_kafka_consumergrouplist_list()
+    get_consumers_topics(get_consumer_client_host())
 
 
 if __name__ == '__main__':

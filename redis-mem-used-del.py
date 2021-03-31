134e6d1aaf8d591303e9055c2a6a339a6b1ebc9c
cc <38369051+Holyfiled@users.noreply.github.com>
2021-03-31 09:46:25 +0800

    Create redis-mem-used-del.py

---
+++ redis-mem-used-del.py
@@ -0,0 +1,39 @@
+import redis
+import logging as logger
+logger.basicConfig(filename='', level=logger.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+class Redis:
+    def __init__(self, redis_host, redis_port, redis_auth):
+        try:
+            self.redis_connect_pool = redis.ConnectionPool(host=redis_host, port=redis_port, password=redis_auth, max_connections=10)
+            self.redis_cli = redis.StrictRedis(connection_pool=self.redis_connect_pool)
+            logger.info('Connect redis server success, redis instance info: [%s:%d]' % (redis_host, redis_port))
+        except Exception as e:
+            logger.error('Connect redis server error.')
+
+    def get_mem_used_ratio(self):
+        mem_used = self.redis_cli.info()['used_memory']
+        mem_max = self.redis_cli.info()['maxmemory']
+        mem_used_ratio = int((mem_used / mem_max) * 100)
+        return mem_used_ratio
+
+    def redis_flush_db(self):
+        mem_used_ratio = self.get_mem_used_ratio()
+        print(type(mem_used_ratio), mem_used_ratio)
+        if mem_used_ratio > 90:
+            logger.warn('redis mem used ratio: %s ' % (mem_used_ratio))
+        else:
+            logger.info('redis mem used ratio: %s ' % (mem_used_ratio))
+
+
+def main():
+    redis_host = '192.168.32.128'
+    redis_port = 6379
+    redis_passwd = ''
+    redis = Redis(redis_host, redis_port, redis_passwd)
+    redis.redis_flush_db()
+
+
+if __name__ == '__main__':
+    main()
